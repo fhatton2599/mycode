2ebdbfdb1e655381be81473ce9796204947d1301
fhatton2599 <fhatton2599@gmail.com>
2024-01-05 16:03:18 +0000

    Speed Ticket Calc with other costs

--- speedcalc.py
+++ speedcalc.py
@@ -1,3 +1,15 @@
+def other_costs():
+    acp = 10
+    ems = 20
+    court = 42.50
+
+
+def initial_fine(speed_limit):
+    if speed_limit >= 65:
+        return 42.50
+    else:
+        return 35.0
+
 def speeding_ticket_calculator():
     try:
         speed_limit = int(input("What is the speed limit?"))
@@ -5,20 +17,11 @@ def speeding_ticket_calculator():
         
         if veh_speed <= speed_limit:
             print("Vehicle was within the posted speed limit.")
- 
         else:
             speed_diff = veh_speed - speed_limit
-            pre_fine = speed_diff - 5
-        
-        if pre_fine <= 5
-        
-        else
-           pre_fine 
-
-        if     
-            
-
-            print(f"Your fine is ${fine_speed}")
+            fine_speed = (speed_diff - 5) * 2
+              
+            print(f"Your fine is ${fine_speed}+{acp}+{ems}{court}")
             
     except ValueError:
             print("Invalid input. Please enter valid integer values for speed limit and vehicle speed.")
