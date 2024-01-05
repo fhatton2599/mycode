eb4394116047fea5d41ee1f8f2037229d5037470
fhatton2599 <fhatton2599@gmail.com>
2024-01-05 18:35:20 +0000

    Calc w/ points added

--- speedcalc.py
+++ speedcalc.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
+#Point systems
+def points(speed_diff):
+    if 6 <= speed_diff <= 10:
+        return 2
+    elif 11 <= speed_diff <= 15:
+        return 3
+    elif 16 <= speed_diff <= 25:
+        return 4
+    elif 26 <= speed_diff <= 30:
+        return 5
+    elif speed_diff >= 31:
+        return "5 & PennDoT Hearing"
+    else:
+        return 0
 
+#Define general costs associated with ticket
 def other_costs():
     jcpatj = 10
     ems = 20
     court = 42.50
     return jcpatj, ems, court
 
+#Surcharge changes with speed difference
 def surcharge(speed_diff):
     if 6 <= speed_diff <= 15:
         return 45
@@ -14,6 +30,7 @@ def surcharge(speed_diff):
     elif speed_diff >= 26:
         return 75
 
+#Initial fine depending on speed zone
 def initial_fine(speed_limit):
     if speed_limit >= 65:
         return 42.50
@@ -42,6 +59,7 @@ def speeding_ticket_calculator():
             print(f"ACPATJ: ${jcpatj}")
             print(f"Total: ${total_fine}")
             
+            print(f"Points: {points(speed_diff)}")
     except ValueError:
             print("Invalid input. Please enter valid integer values for speed limit and vehicle speed.")
             
